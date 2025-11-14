993ba17cd7fccc526faf761c1c176deee49e1461
Ananya Jayaprakash <ananyajay14@gmail.com>
2025-11-14 14:11:46 +0530

    Delete ECU3 directory

--- ECU3/timer_0.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_0_H
-#define TIMER_0_H
-
-/*---------------------------------------------------------
- * Timer0 Initialization Prototype
- *---------------------------------------------------------*/
-void init_timer0(void);
-
-#endif /* TIMER0_H */
